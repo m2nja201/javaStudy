bdff585cdcd55b4317fe4936440a168fdc82d7f8
m2nja2 <m2nja201@gmail.com>
2022-01-29 23:47:54 +0900

    01/29 fourth commit (unfinished)

---
+++ loop0129.java
@@ -0,0 +1,13 @@
+// 01/29
+// 이 part도 c++이랑 비슷
+
+public class loop0129{
+	public static void main(String [] args){
+		int for_temp = 0;
+		
+		for (int i=0; i<10; i++){
+			for_temp += i;
+		}
+		System.out.println("i=0~9의 sum : "+for_temp);
+	}
+}
