97e7a6ef58a601a5df0eef4797250666b3f0e915
m2nja2 <m2nja201@gmail.com>
2022-02-13 02:11:05 +0900

    0213 java study - polymorphism & algorithm

--- part4/poly2_0212.java
+++ part4/poly2_0212.java
@@ -6,27 +6,32 @@ public class poly2_0212{
 		// Car c1 = new Car(); 추상 클래스는 객체를 생성할 수 없지만
 		//Car c2 = new SportsCar();  // SportsCar의 객체로 생성해주면 가능!
 		
-		Car[] cars = new Car[2];
-		cars = new Car[] {new Ambulance(), new SportsCar()};
+		Car[] cars = {new Ambulance(), new SportsCar()};
+		for (int i=0; i<cars.length; i++){
+			cars[i].run();
+		}
+		
+		// for문 업그레이드 버전
+		System.out.println("-----------");
+		for (Car i:cars){
+			i.run();
+		}
 	}
 }
 
-// abstract class Car{
-	// abstract void run();
-// }
-
-class Car{
-	void run();
+abstract class Car{
+	abstract void run();
 }
+
 
 class SportsCar extends Car{
 	void run(){
-		System.out.println("overidding");
+		System.out.println("부아아앙");
 	}
 }
 
 class Ambulance extends Car{
 	void run(){
-		
+		System.out.println("삐용삐용");
 	}
 }
