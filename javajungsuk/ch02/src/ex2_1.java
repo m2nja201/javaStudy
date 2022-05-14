24ce194fcc4f211c23d24e32e61eb001687d4189
W0NM2NJA2 <80443295+m2nja201@users.noreply.github.com>
2022-05-15 01:49:35 +0900

    Add files via upload

---
+++ javajungsuk/ch02/src/ex2_1.java
@@ -0,0 +1,12 @@
+
+public class ex2_1 {
+	public static void main(String[] args) {
+		System.out.printf("%d\n", 15);
+		System.out.printf("%#o\n", 15);
+		System.out.printf("%#x\n", 15);
+		System.out.printf("%s\n", Integer.toBinaryString(15));
+		
+		float f = 123.45678f;
+		System.out.printf("%f\n", f);
+	}
+}
---
+++ javajungsuk/ch02/src/ex2_2.java
@@ -0,0 +1,16 @@
+
+public class ex2_2 {
+	public static void main(String[] args) {
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5/3);
+		System.out.println(5%3);
+		System.out.println(5*3);
+		
+		int x, y;
+		x = 10;
+		y = 20;
+		System.out.println(x/y + x%y);
+		System.out.println("5+3");
+	}
+}
