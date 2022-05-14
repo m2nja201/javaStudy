e8aece127054ac3f3518747598d91bc1f7dfa181
W0NM2NJA2 <80443295+m2nja201@users.noreply.github.com>
2022-05-14 23:48:37 +0900

    Add files via upload

---
+++ javajungsuk/ch01/src/Start.java
@@ -0,0 +1,18 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Staaart the JAVA!");
+		System.out.println("Staaart the JAVA! - copy");
+		// ctrl+shift+L : 단축키 목록
+		// ctrl+d : 한줄 삭제
+		// ctrl+alt+down : 행단위 복사
+		// alt+shift+a : 멀티 칼럼 편집 시작 / 해제
+		// alt+up, alt+down : 행단위 이동
+		// ctrl+i : 자동으로 들여쓰기 해줌
+		// ctrl+/ : 주석
+		
+	}
+
+}
